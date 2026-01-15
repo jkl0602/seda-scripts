27164555b4e7c06e0a4cafa082d10bfe9c56e6e2
jkl0602 <jkl0602@test-seconddive.shop>
2026-01-15 18:35:41 +0900

    package and test game

--- Scripts/Batch/PackageTest.bat
+++ Scripts/Batch/PackageTest.bat
@@ -1,3 +1,3 @@
 call %~dp0Common\CommonEnv.bat
 
-%ENGINE_BATCH_PATH%\RunUAT BuildCookRun -project=%GAME_PATH%\Elpis.uproject -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -clean -build -compile -cook -stage -compressed -pak -archive -archivedirectory=C:\JK\tmp\build
+%ENGINE_BATCH_PATH%\RunUAT BuildCookRun -project=%GAME_PATH%\Elpis.uproject -noP4 -platform=Win64 -clientconfig=Shipping -serverconfig=Shipping -clean -build -compile -cook -stage -compressed -pak -iterate -maps=/Game/Covenant/BG/Level/Sector/LV_Sector_01.umap -archive -archivedirectory=C:\JK\tmp\build
--- Scripts/Batch/TestGame.bat
+++ Scripts/Batch/TestGame.bat
@@ -1,5 +1,5 @@
 call %~dp0Common\CommonEnv.bat
 
 cd %ENGINE_PATH%
-UnrealEditor.exe %GAME_PATH%\Elpis.uproject /Game/Covenant/BG/Level/Sector/LV_Sector_01_FoliageTest -game
+UnrealEditor.exe %GAME_PATH%\Elpis.uproject -game -trace=default,AssetLoadTime
 rem -Windowed -ResX=1280 -ResY=720
