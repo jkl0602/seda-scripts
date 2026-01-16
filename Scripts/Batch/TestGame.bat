ccf7c1733d8e8c24ff01ee50ef51ddd5b0cdc950
jkl0602 <jkl0602@test-seconddive.shop>
2026-01-16 16:10:59 +0900

    add trace channel

--- Scripts/Batch/TestGame.bat
+++ Scripts/Batch/TestGame.bat
@@ -1,5 +1,5 @@
 call %~dp0Common\CommonEnv.bat
 
 cd %ENGINE_PATH%
-UnrealEditor.exe %GAME_PATH%\Elpis.uproject -game -trace=default,AssetLoadTime
+UnrealEditor.exe %GAME_PATH%\Elpis.uproject -game -trace=default,default,counter,stats,file,loadtime,assetloadtime,task
 rem -Windowed -ResX=1280 -ResY=720
